410297f33ac49d8bc302d8aefbd4df51584708e7
MarkosMuche <markosmuche2018@gmail.com>
2022-10-05 22:48:28 +0300

    change annotation to hold the whole id image.

--- Readme.md
+++ Readme.md
@@ -2,3 +2,6 @@
 
 The thing that takes much time here is, installing tensorflow-object-detection-api. You have to read the docs and install it.
 https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/install.html#tf-models-install-object-detection
+
+
+python model_main_tf2.py --model_dir=models/my_ssd_resnet50_v1_fpn --pipeline_config_path=models/my_ssd_resnet50_v1_fpn/pipeline.config
--- detect_img.py
+++ detect_img.py
@@ -14,8 +14,8 @@ wd = os.getcwd()
 #MODEL_PATH = os.path.join(wd,'model_graph','new_graph','saved_model');
 #LABELS_PATH = os.path.join(wd,'Annotations','label_map.pbtxt');
 
-MODEL_PATH = "/media/mark/New Volume/projects/Ben/ID-card-detection-project/results/Trained_model/my_faster_rcnn" # add "model_graph" path from the drive
-LABELS_PATH = "" # add the "Annotations" folder path 
+MODEL_PATH = "/media/mark/New Volume/projects/Ben/ID-card-detection-project/TensorFlow/workspace/training_demo/exported_models/saved_model" # add "model_graph" path from the drive
+LABELS_PATH = "/media/mark/New Volume/projects/Ben/ID-card-detection-project/TensorFlow/workspace/training_demo/annotations/label_map.pbtxt" # add the "Annotations" folder path 
 
 # load the trained model onto memory
 detection_model = tf.saved_model.load(MODEL_PATH)
@@ -44,7 +44,6 @@ def single_model_inference(model,image):
     output_dict['num_detections'] = num_detection
     # detection classes must be an integer
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-    
     
     return output_dict;
 
@@ -86,7 +85,7 @@ def show_inference(model,image_np):
 #            os.path.join(wd,"Test","IMG14.jpg")
             
 #            ]
-img_path = [] # add all your image path here Example : "E:\Train\IMG22.jpg"
+img_path = ['TensorFlow/workspace/training_demo/images/test/0_32895380.jpg',] # add all your image path here Example : "E:\Train\IMG22.jpg"
             
 i = 0
 for p in img_path:
